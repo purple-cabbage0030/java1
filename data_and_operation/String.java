63615cf75877489e2f7ae2361078bc7d51afa5ab
Hongju Lee <litehj84@gmail.com>
2021-11-22 09:58:20 +0900

    Add files via upload

---
+++ data_and_operation/String.java
@@ -0,0 +1,18 @@
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character - 한 글자/문자 
+		
+		// new line -- \n
+		System.out.println("Hello "
+				+ "World");
+		System.out.println("Hello \nWorld");
+		
+		// escape 역슬래쉬 
+		System.out.println("Hello \"World\"");
+
+	}
+
+}
---
+++ data_and_operation/StringOperation.java
@@ -0,0 +1,14 @@
+
+public class StringOperation {
+
+	public static void main(java.lang.String[] args) {
+		
+		// length()
+		System.out.println("Hello World".length());
+		
+		// replace()
+		System.out.println("Hello World".replace("World", "Hello"));
+
+	}
+
+}
