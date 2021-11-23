6efb220598754e21504d393acaf71c0ae0bc6a64
Hongju Lee <litehj84@gmail.com>
2021-11-23 13:39:23 +0900

    Add files via upload

---
+++ data_and_operation/string.java
@@ -0,0 +1,18 @@
+
+public class string {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character - 한 글자/문자 
+		
+		// new line -- \n
+		System.out.println("Hello "
+				+ "World");
+		System.out.println("Hello \nWorld");
+		
+		// escape 역슬래쉬 
+		System.out.println("Hello \"World\"");
+		System.out.println("a\bb\bc");
+	}
+
+}
